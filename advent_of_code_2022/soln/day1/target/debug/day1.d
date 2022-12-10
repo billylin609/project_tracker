207cc57096fb049ae410ed15d6591b56f255999d
billylin609 <linyuchen830@gmail.com>
2022-12-09 23:46:57 -0500

    wrong aoc day1

---
+++ advent_of_code_2022/soln/day1/target/debug/day1.d
@@ -0,0 +1,1 @@
+C:\Users\linyu\project_tracker\advent_of_code_2022\soln\day1\target\debug\day1.exe: C:\Users\linyu\project_tracker\advent_of_code_2022\soln\day1\src\main.rs
---
+++ advent_of_code_2022/soln/day1/target/debug/deps/day1-3e3364b3c8f24c1b.d
@@ -0,0 +1,5 @@
+C:\Users\linyu\project_tracker\advent_of_code_2022\soln\day1\target\debug\deps\day1-3e3364b3c8f24c1b.rmeta: src\main.rs
+
+C:\Users\linyu\project_tracker\advent_of_code_2022\soln\day1\target\debug\deps\day1-3e3364b3c8f24c1b.d: src\main.rs
+
+src\main.rs:
